ee0675ca8f3ff10b49c5e5d83ed926f44c9223dd
Matthew Idungafa <mattidungafa@gmail.com>
2024-04-25 18:45:23 +0100

    add products model added

--- express/models/product.js
+++ express/models/product.js
@@ -1,3 +1,5 @@
+const path = require('path')
+const fs = require('fs')
 const products = []
 
 module.exports = class Product {
@@ -6,7 +8,21 @@ module.exports = class Product {
   }
 
   save() {
-    products.push(this)
+    const p = path.join(
+      path.dirname(process.mainModule.filename),
+      'data',
+      'products.json'
+    )
+    fs.readFile(p, (err, fileContent) => {
+      let products = []
+      if (!err) {
+        products = JSON.parse(fileContent)
+      }
+      products.push(this)
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err)
+      })
+    })
   }
 
   static fetchAll() {
