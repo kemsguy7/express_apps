3c553da0b31ced5a6f279afcbaac3c3cbe3c0497
Matthew Idungafa <mattidungafa@gmail.com>
2024-04-27 15:34:03 +0100

    updates

---
+++ express/models/cart.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(
+    path.dirname(process.mainModule.filename),
+    'data',
+    'cart.json'
+);
+
+module.exports = class Cart {
+    static addProduct(id) {
+        // Fetch the previous cart
+
+        //analyse the cart => Find existing product
+        // Add new product / increase quantity
+
+    }
+}
