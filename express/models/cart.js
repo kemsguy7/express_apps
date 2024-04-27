23add866d4762444948be570af9d59e95c8b2fd2
Matthew Idungafa <mattidungafa@gmail.com>
2024-04-27 16:29:52 +0100

    added cart functionality

--- express/models/cart.js
+++ express/models/cart.js
@@ -8,10 +8,35 @@ const p = path.join(
 );
 
 module.exports = class Cart {
-    static addProduct(id) {
+    static addProduct(id, productPrice) {
         // Fetch the previous cart
+        fs.readFile(p, (err, fileContent) => {
+            let cart = { products: [], totalPrice: 0};
+            if (!err) {
+                cart = JSON.parse(fileContent); 
+            }
+            // Analyze the cart => Find existing product or not
+            const exixtingProductIndex = cart.products.findIndex(
+                prod => prod.id === id
+            );
+            const existingProduct = cart.products[existingProductIndex];
+            let updatedProduct;
+            // Add new product / increase quantity
+            if (existingProduct) {
+                updatedProduct = {...existingProduct};
+                updatedProduct.qty = updatedProduct.qty + 1;
+                cart.products = [...cart.products];
+                cart.products[existingProductIndex] = updatedProduct;
+            } else {
+                updatedProduct = { id: id, qty: 1};
+                cart.products = [...cart.products, updatedProduct];
+            }
+            cart.totalPrice = cart.totalPrice + productPrice;
+            fs.writeFile(p, JSON.stringify(cart), err => {
+                console.log(err);
+            })
+        })
 
-        //analyse the cart => Find existing product
         // Add new product / increase quantity
 
     }
