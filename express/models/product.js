5e5287f6121b11d1bd90adce001179da3bbb691f
Matthew Idungafa <mattidungafa@gmail.com>
2024-04-29 15:43:49 +0100

    updates

--- express/models/product.js
+++ express/models/product.js
@@ -18,7 +18,8 @@ const getProductsFromFile = cb => {
 };
 
 module.exports = class Product {
-  constructor(title, imageUrl, description, price) {
+  constructor(id, title, imageUrl, description, price) {
+    this.id = id;
     this.title = title;
     this.imageUrl = imageUrl;
     this.description = description;
@@ -26,8 +27,13 @@ module.exports = class Product {
   }
 
   save() {
-    this.id = Math.floor(Math.random().toString()); 
     getProductsFromFile(products => {
+      if (this.id) {
+        const existingProductIndex = products.findIndex(prod => prod.id === this.id);
+        const updatedProducts = [...products];
+        updatedProducts[existingProductIndex] = this;
+      }
+      this.id = Math.floor(Math.random().toString()); 
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), err => {
         console.log(err);
