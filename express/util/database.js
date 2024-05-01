220939900df2634d8169c2a3a4a9b3475dde6f0f
Matthew Idungafa <mattidungafa@gmail.com>
2024-05-01 12:31:17 +0100

    updates

---
+++ express/util/database.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'node',
+    password: ''
+}); 
+
+module.exports = pool.promise();  //this will allow use to promises to handle async code
