96ed67f7b868e8ada116866fabb3ee2795d00dee
Matthew Idungafa <mattidungafa@gmail.com>
2024-05-03 17:20:44 +0100

    added product delete functiinality

--- express/controllers/admin.js
+++ express/controllers/admin.js
@@ -22,6 +22,7 @@ exports.postAddProduct = (req, res, next) => {
   .then(result => {
    // console.log(result);
    console.log('Created Product');
+   res.redirect('/admin/products');
   })
   .catch(err => {
     console.log(err);
@@ -94,8 +95,17 @@ exports.getProducts = (req, res, next) => {
     console.log(err);
   });
 };
-exports.postDeleteProduct = (req, res, next) => {
+
+exports.postDeleteProduct = (req, res, next) => { //admin delete product functionality
   const prodId = req.body.productId;
-  Product.deleteById(prodId);
-  res.redirect('/admin/products');
+  Product.findByPk(prodId)
+  .then(product => {
+    return product.destroy();
+  })
+  .then(result => {
+    console.log('DESTROYED PRODUCT');
+    res.redirect('/admin/products')
+  })
+  .catch(err => console.log(err));
+  
 };
