1943f5c348ffce64e924d656f3e5e502b865ab91
Matthew Idungafa <mattidungafa@gmail.com>
2024-05-04 11:18:39 +0100

    updates

--- express/controllers/admin.js
+++ express/controllers/admin.js
@@ -13,7 +13,7 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  req.user.createProduct({ //sequelize method to create a new product
+  req.user.createProduct({ //sequelize method to create a new product, this automatically creates a product with the user id
     title: title,
     price : price, 
     imageUrl: imageUrl, 
