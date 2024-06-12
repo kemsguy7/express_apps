e93a1262d7da7415a92985718d83370e014697c9
kemsguy7 <31693594+kemsguy7@users.noreply.github.com>
2024-06-12 07:51:34 +0100

    Update database.js

--- mongo_app/util/database.js
+++ mongo_app/util/database.js
@@ -2,5 +2,3 @@ const mongodb = require('mongodb');
 
 const MongoClient = mongodb.MongoClient; // This is a constructor function that we can use to create a new client
 
-
-MongoClient.connect('mongodb+srv://mattidungafa:zwLBWV7NRxwq2W2l@cluster0.grapefc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
