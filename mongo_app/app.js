1fbadc73aedd441f72c49414be51be2587a968c7
Matthew Idungafa <mattidungafa@gmail.com>
2024-06-13 14:18:09 +0100

    updates

--- mongo_app/app.js
+++ mongo_app/app.js
@@ -1,20 +1,22 @@
- const path = require('path');
+const path = require('path')
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require('express')
+const bodyParser = require('body-parser')
 
-const errorController = require('./controllers/error');
+const errorController = require('./controllers/error')
 
-const app = express();
+const mongoConnect = require('./util/database')
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+const app = express()
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+app.set('view engine', 'ejs')
+app.set('views', 'views')
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use((req, res, next) => {
   // User.findById(1)
@@ -23,10 +25,14 @@ app.use((req, res, next) => {
   //     next();
   //   })
   //   .catch(err => console.log(err));
-});
+})
 
-app.use('/admin', adminRoutes);
-app.use(shopRoutes);
+app.use('/admin', adminRoutes)
+app.use(shopRoutes)
 
-app.use(errorController.get404);
+app.use(errorController.get404)
 
+mongoConnect((client) => {
+  console.log(client)
+  app.listen(3000)
+})
--- mongo_app/util/database.js
+++ mongo_app/util/database.js
@@ -1,6 +1,18 @@
-const mongodb = require('mongodb');
+const mongodb = require('mongodb')
 
-const MongoClient = mongodb.MongoClient; // This is a constructor function that we can use to create a new client
+const MongoClient = mongodb.MongoClient // This is a constructor function that we can use to create a new client
 
+const mongoConnect = (callback) => {
+  MongoClient.connect() //add mongodb url here
+    .then((client) => {
+      console.log('Connected!')
+      callback(result)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+}
 
-MongoClient.connect('mongodb+srv://mattidungafa:zwLBWV7NRxwq2W2l@cluster0.grapefc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+module.exports = mongoConnect
+
+// Connect to database
