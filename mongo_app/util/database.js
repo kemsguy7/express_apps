7e59bb8e49e727c3d5c3796ff94f1c3796b394d9
Matthew Idungafa <mattidungafa@gmail.com>
2024-06-13 15:59:24 +0100

    updates

--- mongo_app/util/database.js
+++ mongo_app/util/database.js
@@ -1,18 +1,28 @@
 const mongodb = require('mongodb')
-
 const MongoClient = mongodb.MongoClient // This is a constructor function that we can use to create a new client
+
+let _db //define an empty variable to store the database connection
 
 const mongoConnect = (callback) => {
   MongoClient.connect() //add mongodb url here
     .then((client) => {
       console.log('Connected!')
-      callback(result)
+      _db = client.db()
+      callback(client)
     })
     .catch((err) => {
       console.log(err)
     })
 }
 
+const getDb = () => {
+  if (_db) {
+    return _db
+  }
+  throw 'No database found!'
+}
+
+exports.mongoConnect = mongoConnect
 module.exports = mongoConnect
 
 // Connect to database
