87eff6ebfcc8be4404a86c3cc5d329fe6d8a4fae
Matthew Idungafa <mattidungafa@gmail.com>
2024-06-13 14:34:19 +0100

    updates to package.json files

--- mongo_app/util/database.js
+++ mongo_app/util/database.js
@@ -3,7 +3,9 @@ const mongodb = require('mongodb')
 const MongoClient = mongodb.MongoClient // This is a constructor function that we can use to create a new client
 
 const mongoConnect = (callback) => {
-  MongoClient.connect() //add mongodb url here
+  MongoClient.connect(
+    'mongodb+srv://mattidungafa:zwLBWV7NRxwq2W2l@cluster0.grapefc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+  ) //add mongodb url here
     .then((client) => {
       console.log('Connected!')
       callback(result)
