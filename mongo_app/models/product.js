315e6a156c68e19e7f4edac41c02714a06418e10
Matthew Idungafa <mattidungafa@gmail.com>
2024-06-29 00:04:02 +0100

    edit functionality completed in mongodb

--- mongo_app/models/product.js
+++ mongo_app/models/product.js
@@ -2,20 +2,29 @@ const mongoDb = require('mongodb')
 const getDb = require('../util/database').getDb
 
 class Product {
-  constructor(title, price, description, imageUrl) {
+  constructor(title, price, description, imageUrl, id) {
     this.title = title
     this.price = price
     this.description = description
     this.imageUrl = imageUrl
+    this._id = id
   }
 
   save() {
     const db = getDb()
+    let dbOp
+    if (this._id) {
+      //update the product
+      dbOp = db
+        .collection('products')
+        .updateOne({ _id: new mongoDb.ObjectId(this._id) }, { $set: this })
+    } else {
+      dbOp = db.collection('products').insertOne(this)
+    }
 
-    return db
-      .collection('products') // Ensure to return the promise
-      .insertOne(this)
+    return dbOp
       .then((result) => {
+        4
         console.log(result)
       })
       .catch((err) => {
